fe004f0ffb592f28be54f6a73350b0c754d1e9d5
circld <circld1@gmail.com>
2025-06-01 17:04:15 -0700

    add ctrl-f fullscreen keybind to zellij

--- managed/zellij.nix
+++ managed/zellij.nix
@@ -1,8 +1,21 @@
 {
   pkgs,
   config,
+  lib,
   ...
 }:
+# workaround while home manager config -> kdl gets hashed out
+# https://github.com/nix-community/home-manager/issues/4659#issuecomment-2574906923
+let
+  binds = bindings:
+    lib.attrsets.mapAttrs' (key: value: lib.attrsets.nameValuePair "bind \"${key}\"" value) bindings;
+  unbinds = unbindList: let
+    len = builtins.length unbindList;
+    sub = lib.lists.sublist 0 (len - 1) unbindList;
+  in {
+    ${"unbind" + (lib.strings.concatStrings (lib.lists.map (x: " \"${x}\"") sub))} = lib.lists.last unbindList;
+  };
+in
 {
   programs.zellij = {
     enable = true;
@@ -11,6 +24,11 @@
       default_shell = "fish";
       mouse_mode = true;
       theme = "space-vim-dark";
+      keybinds = {
+        normal = binds {
+          "Ctrl f" = { ToggleFocusFullscreen = []; };
+        };
+      };
     };
     themes = {
       "space-vim-dark" = ''
